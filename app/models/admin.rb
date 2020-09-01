29f1aa46c5135e2c301312c4cb9bebd4a5e99bca
abdul133 <abdul.rahman@square63.org>
2020-09-01 17:02:14 +0500

    first task

---
+++ app/models/admin.rb
@@ -0,0 +1,10 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
+
+         validates_presence_of :first_name
+         validates_presence_of :last_name
+         validates_presence_of :phone_number
+end
---
+++ test/controllers/home_page_admins_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HomePageAdminsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
