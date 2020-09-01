648f44565b74f724b954bcec9a171011f4271bc1
abdul133 <abdul.rahman@square63.org>
2020-09-01 17:02:14 +0500

    first task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  	root 'home_page_admins#index'
 end
