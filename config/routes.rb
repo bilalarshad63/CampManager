50bb8a6579c56a5e6a30ab43c828618f55fa8801
bilalarshad63 <69192544+bilalarshad63@users.noreply.github.com>
2020-09-04 12:11:49 +0500

    password functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-
-
   devise_for :admins
   root 'home#index'
-  devise_for :users, controllers: {registrations: "registrations"}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  devise_for :users, controllers: {registrations: "registrations", passwords: "passwords"}
   resources :privacy, only: [:index]
-
   delete 'users/:id(.:format)/destroy', :to => 'home_page_admins#destroy' , as: 'destroy_user'
   get '/admin', :to => 'home_page_admins#index' , as: 'homepage'
   get '/admin/users/:id(.:format)/show', :to => 'home_page_admins#show_user' , as: 'show_user'
-
-	
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
 end
