bc2ee53697a3e9aa1bbb9557dff0ffc0abe3b179
abdul133 <abdul.rahman@square63.org>
2020-09-18 02:10:51 +0500

    devise separated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+ 
   devise_for :users, controllers: { registrations: 'registrations', passwords: 'passwords' }
   resources :users
   resources :camp_applications do
@@ -7,17 +7,20 @@ Rails.application.routes.draw do
       get 'camp/:camp_id/profile', to: 'camp_applications#profile', as: 'profile'
     end
   end
-  
+ 
   resources :privacy, only: [:index]
-  
+ 
   resources :camp_locations do
     collection do
       get 'export_csv'
     end
   end
 
-  devise_for :admins, controllers: { registrations: 'admin/registrations', sessions: 'admin/sessions' }
+  devise_for :admins, controllers: { registrations: 'devise_admin/registrations', sessions: 'devise_admin/sessions' }
   resources :admins
+
+
+  
   get '/admin', to: 'home_page_admins#index', as: 'homepage'
   get '/admin/users/new', to: 'home_page_admins#new_user', as: 'new_user_by_admin'
   post '/admin/users/create', to: 'home_page_admins#create_user', as: 'create_user_by_admin'
