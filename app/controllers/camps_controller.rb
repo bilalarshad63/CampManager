379b8193993b83f53378c4ba624982594fe10714
bilalarshad63 <69192544+bilalarshad63@users.noreply.github.com>
2020-09-18 16:20:29 +0500

    feedback task

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -87,10 +87,7 @@ class CampsController < ApplicationController
   end
 
   def new_camp_application
-    @camp_application = CampApplication.new
-    @camp_application.user_id = current_user.id
-    @camp_application.camp_id = @camp.id
-    @camp_application.save
+    @camp_application = CampApplication.create(user_id: current_user.id, camp_id: @camp.id)
   end
 
   def camp_params
