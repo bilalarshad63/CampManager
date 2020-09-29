0ed8ff93c38a4cb9bc2480883059ba14fdeda1ce
bilalarshad63 <69192544+bilalarshad63@users.noreply.github.com>
2020-09-29 11:25:20 +0500

    commented out verify authentication token

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,7 +1,7 @@
 
 class BaseController < ActionController::API
 
-	skip_before_action :verify_authenticity_token
+	#skip_before_action :verify_authenticity_token
 
   before_action :set_default_format
   before_action :authenticate_token!
